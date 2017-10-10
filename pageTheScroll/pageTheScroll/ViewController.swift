433f05f0cd98734afd5ff1169fe5a5da2dfbf20c
amagdy529 <amagdy529@gmail.com>
2017-10-09 17:34:38 -0700

    some changes

--- pageTheScroll/pageTheScroll/ViewController.swift
+++ pageTheScroll/pageTheScroll/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var images 	= [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        for x in 0...2 {
+            let image = UIImage(named: "icon:\(x).png")
+            images.append(UIImageView(image: image))
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
