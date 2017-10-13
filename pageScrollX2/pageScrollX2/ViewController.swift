41c2377e7417eab1026c8267f0e5d217ff775a66
amagdy529 <amagdy529@gmail.com>
2017-10-13 07:23:31 -0700

    added the fuckin images but still there is a fuckin thread and error

--- pageScrollX2/pageScrollX2/ViewController.swift
+++ pageScrollX2/pageScrollX2/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollViewX: UIScrollView!
+    var images = [UIImageView]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Scrollview width: \(scrollViewX.frame.size.width)")
+
+        let scrollWidth = scrollViewX.frame.size.width
+
+        for x in 0...2 {
+            let imageX = UIImage(named: "icon\(x).png")
+            let imageViewX = UIImageView(image: imageX)
+            images.append(imageViewX)
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            
+            
+            scrollViewX.addSubview(imageViewX)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
